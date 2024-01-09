a8153b7ef046d17718288cb311e99a3d9fa7da1c
Steve245270533 <245270533@qq.com>
2024-01-09 09:47:56 +0800

    refactor(tsconfig.json): change module resolution strategy

--- src/typings/RoundedBoxGeometry.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "three/examples/jsm/geometries/RoundedBoxGeometry" {
-	import {BoxGeometry} from "three";
-
-	export class RoundedBoxGeometry extends BoxGeometry {
-		constructor(width?: number, height?: number, depth?: number, segments?: number, radius?: number);
-	}
-}
